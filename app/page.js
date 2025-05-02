e99529fa294f2d2b07121fce8d5a6e854bcd3210
FUKS2025 <jiro950117@gmail.com>
2025-05-02 20:16:53 +0800

    Delete app directory

--- app/page.js
+++
@@ -1,7 +0,0 @@
-export default function BarberLABPage() {
-  return (
-    <div className="bg-black text-white min-h-screen font-sans flex items-center justify-center">
-      <h1 className="text-4xl font-bold">BarberLAB is live!</h1>
-    </div>
-  );
-}
